c8cb1720d7f7184c3c83173cae983ce7da91c3a4
Ilya Ermakov <ermakoff.ilya@outlook.com>
2025-07-09 18:59:30 +0300

    Add some plots

--- hyperelasticity_postproc.jl
+++ hyperelasticity_postproc.jl
@@ -56,15 +56,22 @@ temp = data[:, 15]
 
 eos = Stiffened()
 pres = [-1 / 3 * tr(strs[i]) for i in eachindex(strs)]
-println("Calculating temperature...")
+# println("Calculating temperature...")
 # temp2 = [300 * exp(ent[i] / (9.3e-4) - 2 * (2.78 / den[i])) for i in eachindex(ent)]
 X = range(0, 1, length=length(ent))
 println("Generated X grid with $(length(X)) points.")
 
-const TITLES = ["Плотность", "Скорость X", "Скорость Y", "Скорость Z",
-  "Энтропия", "Давление", "Температура"]
-const YLABELS = ["ρ, г/см^3", "u_x, км/c", "u_y, км/c", "u_z, км/c",
-  "η, кДж/(г K)", "P, Па", "Θ, K"]
+const XYZ = ["X", "Y", "Z"]
+const xyz = [lowercase(l) for l in XYZ]
+
+const TITLES = ["Плотность", ["Скорость $(XYZ[i])" for i in 1:3]...,
+  "Энтропия", "Давление", "Температура",
+  ["Напряжение $(XYZ[i])$(XYZ[j])" for i in 1:3 for j in i:3]...,
+ ]
+const YLABELS = ["ρ, г/см^3", ["u_$(xyz[i]), км/c" for i in 1:3]...,
+  "η, кДж/(г K)", "P, ГПа", "Θ, K",
+  ["σ_$(xyz[i])$(xyz[j]), ГПа" for i in 1:3 for j in i:3]...
+  ]
 const COLORS = [:blue, :black]
 
 function plot_and_save(x, y, title_str, ylabel_str, filename, color)
@@ -83,17 +90,18 @@ function plot_and_save(x, y, title_str, ylabel_str, filename, color)
   ylabel!(p, ylabel_str)
   mkpath(PLOTPATH)
   savefig(p, PLOTPATH * filename)
-  println("Saved lot to $(PLOTPATH * filename)")
+  println("Saved plot to $(PLOTPATH * filename)")
 end
 
 plots = [
   (den, TITLES[1], YLABELS[1], "density.png"),
-  [(vel[:, i], TITLES[i+1], YLABELS[i+1], "velocity_1.png") for i in 1:3]...,
-  # (vel[:, 1], TITLES[2], YLABELS[2], "velocity_1.png"),
-  # (vel[:, 2], TITLES[3], YLABELS[3], "velocity_2.png"),
-  # (vel[:, 3], TITLES[4], YLABELS[4], "velocity_3.png"),
+  [(vel[:, i], TITLES[i+1], YLABELS[i+1], "velocity_$i.png") for i in 1:3]...,
   (ent, TITLES[5], YLABELS[5], "entropy.png"),
   (pres, TITLES[6], YLABELS[6], "pressure.png"),
+  (temp, TITLES[7], YLABELS[7], "temperature.png"),
+  [([el[1, i] for el in strs], TITLES[7+i], YLABELS[7+i], "tension_1$i.png") for i in 1:3]...,
+  [([el[2, i] for el in strs], TITLES[9+i], YLABELS[9+i], "tension_2$i.png") for i in 2:3]...,
+  ([el[3, 3] for el in strs], TITLES[13], YLABELS[13], "tension_33.png")
 ]
 for (y, title, ylabel, fname) in plots
   plot_and_save(X, y, title, ylabel, fname, COLORS[1])
